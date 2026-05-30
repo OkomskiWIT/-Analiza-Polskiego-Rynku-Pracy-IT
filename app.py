10860e2b2952893b4b159d30d365d80e1915dcb2
OkomskiWIT <okomski@wit.edu.pl>
2026-05-30 09:12:23 +0200

    Some layout improvements

--- app.py
+++ app.py
@@ -37,11 +37,19 @@ def fetch_poland_data():
     df = pd.read_sql("SELECT * FROM poland_job_offers;", engine)
     
     if not df.empty:
-        # Sortujemy dane po dacie dodania, aby najnowsze oferty były na górze
+        # 1. Sortujemy dane po dacie dodania, aby najnowsze oferty były na górze
         if 'date_added' in df.columns:
             df = df.sort_values(by='date_added', ascending=False)
-        # Usuwamy duplikaty na podstawie kluczowych kolumn, zachowując najnowszy wpis
-        df = df.drop_duplicates(subset=['title', 'company_name', 'location', 'salary_min', 'salary_max'], keep='first')
+            
+        # 2. Tworzymy tymczasowe kolumny do porównywania
+        df['temp_title'] = df['title'].astype(str).str.lower().str.replace(r'[^a-z0-9]', '', regex=True)
+        df['temp_company'] = df['company_name'].astype(str).str.lower().str.replace(r'(sp\. z o\.o\.|spółka|inc\.|ltd\.|sa|s\.a\.|[^a-z0-9])', '', regex=True)
+        
+        # 3. Usuwamy duplikaty
+        df = df.drop_duplicates(subset=['temp_title', 'temp_company'], keep='first')
+        
+        # 4. Sprzątamy po sobie, usuwając kolumny tymczasowe
+        df = df.drop(columns=['temp_title', 'temp_company'])
         
         df['salary_avg'] = (df['salary_min'] + df['salary_max']) / 2
         
